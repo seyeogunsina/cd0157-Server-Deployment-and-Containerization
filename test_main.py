2f12defed31fe5f110917f8b9a36d30d5ae8bb29
Adeseye Ogunsina <adeseyeogunsina@gmail.com>
2022-09-02 20:43:16 +0100

    final push with tests passed

--- test_main.py
+++ test_main.py
@@ -25,8 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    #assert response.json == 'Healthy'
-    assert response.json == ''
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
